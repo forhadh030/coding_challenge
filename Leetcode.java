7dff28b8eba544a3476b5a5511a3a176dc4b3f1c
Syed Forhad Hussain <syedf.hussain7@gmail.com>
2023-07-18 09:32:36 -0400

    Update Problem
    Added comment for better understanding

--- Leetcode.java
+++ Leetcode.java
@@ -1,26 +1,38 @@
-package org.example;
-
 public class Leetcode {
     public static void main(String[] args) {
 
+        // Input string
         String s = "I am from Bangladesh";
-        
+
+        // Convert the string to a character array
         char[] split = s.toCharArray();
+        
+        // Pointers for swapping characters
         int left = 0;
         int right = split.length - 1;
         char temp;
-        while(left <= right) {
-            if(split[left] == ' ') {
+
+        // Reverse the words in the string
+        while (left <= right) {
+            // Move the left pointer if a space is encountered
+            if (split[left] == ' ') {
                 left++;
-            } else if(split[right] == ' ') {
+            }
+            // Move the right pointer if a space is encountered
+            else if (split[right] == ' ') {
                 right--;
-            } else {
+            }
+            // Swap characters at left and right positions
+            else {
                 temp = split[left];
                 split[left] = split[right];
                 split[right] = temp;
-                left++; right--;
+                left++;
+                right--;
             }
         }
+        
+        // Convert the character array back to a string and print
         System.out.println(new String(split));
     }
 }
